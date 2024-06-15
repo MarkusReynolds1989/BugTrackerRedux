247d53d139d12d92df2bb40fb70f44c2a8db3558
Markus <markusreynolds1989@gmail.com>
2024-06-15 05:49:00 -0500

    My original setup was all incorrect so I had to redo it.

---
+++ src/BugTrackerRedux/Controllers/TestController.fs
@@ -0,0 +1,23 @@
+﻿namespace BugTrackerRedux.Controllers
+
+open System
+open System.Text
+open System.Security.Cryptography
+open System.Threading.Tasks
+open BugTrackerRedux.Models.User
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Data.Sqlite
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Logging
+open Dapper
+
+[<ApiController>]
+[<Route("api/[controller]")>]
+type TestController(config: IConfiguration, logger: ILogger<LoginController>) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member _.Get(): IActionResult =
+        logger.LogInformation("Hit test endpoint.")
+        let response = {| message = "Test successful"; data = "Dorritos" |}
+        base.Ok(response) :> IActionResult
